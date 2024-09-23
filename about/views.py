05ea2517bd1f0ac7121c33f2ee9a32fd7ed0967c
Daniel Harris <dwh261@gmail.com>
2024-09-23 19:10:28 +0000

    Corrected code in views.py file

--- about/views.py
+++ about/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from hello_world import views as index_views
+from about import views as about_views
 
 # Create your views here.
 def about_me(request):
