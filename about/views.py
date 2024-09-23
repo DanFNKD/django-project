04e767c3997b968aeb87e2051ee3dd2492993f1d
Daniel Harris <dwh261@gmail.com>
2024-09-23 19:08:56 +0000

    tested URLs in django

--- about/views.py
+++ about/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from hello_world import views as index_views
 
 # Create your views here.
+def about_me(request):
+    return HttpResponse("This would be the about page")
