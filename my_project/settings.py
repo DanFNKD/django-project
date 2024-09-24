dae8f5387326b0d0db6961b0b596f36643bcc9bb
Daniel Harris <dwh261@gmail.com>
2024-09-24 18:32:18 +0000

    Testing deployment

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pmf8&e%8xdsd6wp6@c^5ykjnb74od8*2v_r5v0vna5ysaedmm3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-danfnkd-djangoproject-y6gpvdisxdc.ws.codeinstitute-ide.net', '.herokuapp.com']
 
